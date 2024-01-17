54bddff1fa3e001ceb15a0dac366087bade5c388
roemer2201 <roemer2201@users.noreply.github.com>
2024-01-17 21:17:11 +0100

    Update configure-grd.sh: removed typo in "gnome-extensions" command

--- etc/skel/.local/bin/configure-grd.sh
+++ etc/skel/.local/bin/configure-grd.sh
@@ -7,4 +7,4 @@ grdctl rdp set-tls-cert ${GRDCERTDIR}/grd-tls.crt
 grdctl rdp set-tls-key ${GRDCERTDIR}/grd-tls.key
 systemctl --user enable gnome-remote-desktop.service
 systemctl --user restart gnome-remote-desktop.service
-gnome-extension enable allowlockedremotedesktop@kamens.us
+gnome-extensions enable allowlockedremotedesktop@kamens.us
