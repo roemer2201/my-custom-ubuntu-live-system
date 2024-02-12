61f7b7cf28195dd73b6cfa9d1a9b5c0f7a9963b3
roemer2201 <roemer2201@users.noreply.github.com>
2024-02-12 23:39:49 +0100

    Update ubuntu-live-custom.sh: Added ramdisk

--- etc/ubuntu-live-custom.sh
+++ etc/ubuntu-live-custom.sh
@@ -2,3 +2,7 @@
 # Extract last octet of IP and append it to the hostname
 hostnamectl hostname ubuntu-live-ip-$(hostname -I | awk 'BEGIN { FS="[. ]" } ; {print substr($4,0)}')
 systemctl restart smbd
+RAMDISKDIR=/ram
+mkdir $RAMDISKDIR
+chmod +t $RAMDISKDIR
+mount -t tmpfs -o size=80% tmpfs $RAMDISKDIR
