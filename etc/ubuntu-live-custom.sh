de184c285f3c58a0d04ed4ea330e1fecf9174ac7
roemer2201 <roemer2201@users.noreply.github.com>
2025-08-26 22:26:49 +0200

    Update ubuntu-live-custom.sh: New hostname naming scheme using the full ip or part of product_uuid

--- etc/ubuntu-live-custom.sh
+++ etc/ubuntu-live-custom.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-# Extract last octet of IP and append it to the hostname
-hostnamectl hostname ubuntu-live-ip-$(hostname -I | awk 'BEGIN { FS="[. ]" } ; {print substr($4,0)}')
+# configure hostname
+HOSTIP=$(hostname -I | awk '{print $1}' | sed 's/\./-/g')
+if [ -z "$HOSTIP" ]; then
+	# Fallback: product_uuid (ersten 8 Zeichen)
+	if [[ -r /sys/class/dmi/id/product_uuid ]]; then
+		HOSTID=$(cut -c1-8 /sys/class/dmi/id/product_uuid)
+	else
+		HOSTID="no-uuid"
+	fi
+	hostnamectl set-hostname "ucl-$HOSTID"
+else
+	hostnamectl hostname "ucl-$HOSTIP"
+fi
+# restart samba to use the new hostname
 systemctl restart smbd
+# create RAM-disk for convenient temporary storage
 RAMDISKDIR=/ram
 mkdir $RAMDISKDIR
 chmod +t $RAMDISKDIR
